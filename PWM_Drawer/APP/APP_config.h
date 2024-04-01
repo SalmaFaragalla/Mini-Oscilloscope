f4a4ec8d7309f8090361a028ef59b53d08e14ca2
SalmaFaragalla <142256837+SalmaFaragalla@users.noreply.github.com>
2024-04-01 11:19:53 +0200

    Update APP_config.h

--- PWM_Drawer/APP/APP_config.h
+++ PWM_Drawer/APP/APP_config.h
@@ -6,6 +6,7 @@
 #define APP_CONFIG_H_
 
 #include "STM32F103.h"
+#include "../HAL/GLCD/GLCD_interface.h"
 
 /*Timers configurations-----------------------------------------------------*/
 #define APP_TIM_PWM_TIMx (TIM2)
@@ -17,10 +18,10 @@
 #define APP_TIM_IC2_TIMx (TIM1)
 #define APP_TIM_IC2_CHx (TIM_CH2)
 
-#define APP_GLCD_LOW_LINE (7UL)
-#define APP_GLCD_HIGH_LINE (5UL)
-
 /*GLCD configurations-----------------------------------------------------*/
+#define APP_GLCD_LOW_LINE (GLCD_LINE_7)
+#define APP_GLCD_HIGH_LINE (GLCD_LINE_5)
+
 #define APP_GLCD_CYCLE_1_START_X (7UL)
 #define APP_GLCD_CYCLE_2_START_X (APP_GLCD_CYCLE_1_START_X + 60UL)
 #define APP_GLCD_CYCLE_2_END_X (127UL)
